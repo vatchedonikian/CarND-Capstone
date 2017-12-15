795e27726fdc9dfb6edbd5d5754b6372f111176b
fangchun007 <fangchun007@gmail.com>
2017-12-15 04:39:43 -0800

    Update server.py

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -11,7 +11,9 @@ from flask import Flask, render_template
 from bridge import Bridge
 from conf import conf
 
-sio = socketio.Server()
+eventlet.monkey_patch()
+sio = socketio.Server(async_mode='eventlet')
+#sio = socketio.Server()
 app = Flask(__name__)
 msgs = []
 
